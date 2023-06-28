00c4635503d40fac705e8abf6b0ce08f4645210d
AlecRobinGould <argould2277@gmail.com>
2023-06-28 09:49:31 +0200

    new file:   Decorate/customDecorator.py 	modified:   Testing/Multi-thread exitdecorate.py 	modified:   measurements/monitor.py 	modified:   purge.py 	Decorate/timeOuts.ini

---
+++ Decorate/customDecorator.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+"""
+================================================
+
+Requires modules from "List of dependencies.txt"
+================================================
+"""
+from __future__ import print_function
+import sys
+import threading
+from time import sleep
+try:
+    import thread
+except ImportError:
+    import _thread as thread
+
+class customDecorators():
+
+    def __init__(self):
+        pass
+
+    def quitFunction(fnName):
+        # print to stderr, unbuffered in Python 2.
+        print('{0} took too long'.format(fnName), file=sys.stderr)
+        sys.stderr.flush() # Python 3 stderr is likely buffered.
+        # thread.exit()
+        # raise timeOutError()
+        thread.interrupt_main() # raises KeyboardInterrupt
+
+    def exitAfter(**s):
+        '''
+        use as decorator to exit process if 
+        function takes longer than s number seconds
+        '''
+        def outer(fn):
+            def inner(self, *args, **kwargs):
+                timeLimit = self.venttimeout
+                timer = threading.Timer(timeLimit, customDecorators.quitFunction, args=[fn.__name__])
+                timer.start()
+                try:
+                    result = fn(self, *args, **kwargs)
+                finally:
+                    timer.cancel()
+                return result
+            return inner
+        return outer
+
+    """Usage example as a countdown timer"""
+    @exitAfter(3)
+    def countdown(n):
+        print('countdown started', flush=True)
+        for i in range(n, -1, -1):
+            print(i, end=', ', flush=True)
+            sleep(1)
+        print('countdown finished')
